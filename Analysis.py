6108a1ecd55f7f6c3ca51cd1988a3ac3cb9d9f9f
shafra-hub <69393617+shafra-hub@users.noreply.github.com>
2020-10-12 11:58:19 -0400

    Update Analysis.py
    I realized I only saved this to my personal repo yesterday. Oops!

--- Analysis.py
+++ Analysis.py
@@ -44,3 +44,47 @@ tdf['China Count'] = tdf.Transcript.str.count('China')
 wordPivot = tdf.pivot_table(index = ['Speaker_fin'], values = ['China Count'], aggfunc = 'sum') # pivot table aggregates total number of word mentions across a speaker
 # print(wordPivot)
 wordPivot.to_csv('testwords.csv') 
+
+#creation of dataframes by election cycle and by candidate/party
+#Begin 1960
+# create Nixon/GOP dataframe for 1960
+Nixondf = df[df["Speaker_fin"]=='NIXON']
+Nixondf
+
+# create Kennedy/Dem dataframe for 1960
+Kennedydf = df[df["Speaker_fin"]=='KENNEDY']
+Kennedydf # the copyright information gets attributed to Kennedy
+
+# create 1960 both candidates dataframe
+debates_1960= Nixondf.append(Kennedydf)
+debates_1960
+
+#Begin 1976
+#September 23, 1976 debate
+Sept23_1976_debate = df[df["Debate"]=='September 23, 1976 Debate Transcript']
+Sept23_1976_debate
+
+#October 6, 1976 debate
+Oct6_1976_debate = df[df["Debate"]=='October 6, 1976 Debate Transcript']
+Oct6_1976_debate
+
+#October 22, 1976 debate
+Oct22_1976_debate = df[df["Debate"]=='October 22, 1976 Debate Transcript']
+Oct22_1976_debate
+
+#full debate transcript for 1976, includes moderator and any other non-candidate speech
+debates_1976 = Sept23_1976_debate.append([Oct6_1976_debate, Oct22_1976_debate])
+debates_1976
+
+#for Ford/GOP 1976; Ford76 is also what we have for the GOP in 1976 because we don't have the VP debate transcript
+
+Ford76 = debates_1976[(debates_1976["Speaker_fin"]=='FORD')]
+Ford76 #row 810 attributes speech from a Mr. Kraft to Ford
+
+#Carter/Dem 1976 debates; Carter76 is also what we have for Dems in 1976 because we don't have the VP debate transcript
+Carter76 = debates_1976[(debates_1976["Speaker_fin"]=='CARTER')]
+Carter76 # includes (barely audible) in Carter word count
+
+#both candidates/parties 1976 (without moderator/others)
+allcandidates76 = Ford76.append([Carter76])
+allcandidates76
