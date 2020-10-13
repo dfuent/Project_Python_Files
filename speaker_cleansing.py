ff8ec5004bd6aa18d15ae491adcd691e5ebabcd1
Dave <48577149+dfuent@users.noreply.github.com>
2020-10-13 17:53:32 -0400

    Add files via upload
    Fixed fill issue for blank rows

--- speaker_cleansing.py
+++ speaker_cleansing.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct  5 11:56:31 2020
-
 @author: dfuent
 """
 import pandas as pd
@@ -9,7 +8,7 @@ import os
 import numpy as np
 
 def speaker_map(row):
-    
+
     if type(row['Transcript']) is str:
         if row['Transcript'].find(':') > 0:
             f = row['Transcript'].find(':')
@@ -17,7 +16,9 @@ def speaker_map(row):
             f = row['Transcript'].find(';')
         if f > 0:            
             return row['Transcript'][0:f]
-   
+        else:
+            return np.nan
+
 
 df = pd.read_csv('Transcripts_df.csv')
 
@@ -32,7 +33,8 @@ for i in l:
     df['Speaker_Clean'] = df['Speaker_Clean'].str.replace(i, '')
 print(df.head())
 
-df['Speaker_Clean'] = np.where(df['Speaker_Clean'].isnull(), df['Speaker'], df['Speaker_Clean'])
+df['Speaker_Clean'] = df['Speaker_Clean'].fillna(method='ffill')
+#df['Speaker_Clean'] = np.where(df['Speaker_Clean'].isnull(), df['Speaker'], df['Speaker_Clean'])
 df['Speaker_fin'] = np.where(df['Speaker_Clean'].str.split(' ').str.len() > 3, df['Speaker'], df['Speaker_Clean'])
 df['Speaker_fin'] = np.where(df['Speaker_fin'].isnull(), df['Speaker_Clean'], df['Speaker_fin'])
 df.fillna('xx')
@@ -56,5 +58,3 @@ df = df.join(speaker_map.set_index('Speaker_fin'), rsuffix='_map', on = 'Speaker
 #df = df.merge(speaker_map,  on='Speaker_fin', suffixes=('','_map'))
 
 df.to_csv('Transcripts_final.csv')
-
-
